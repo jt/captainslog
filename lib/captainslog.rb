fb3b29fc78b93783a76e99303cd60d2b8d30bb0e
John Thomas Marino <writejm@gmail.com>
2011-04-29 00:49:56 -0400

    Use module instead of class; refactor with shortlog

--- lib/captainslog.rb
+++ lib/captainslog.rb
@@ -1,66 +1,39 @@
 require 'date'
 
 module Captainslog
-  class Runner
-    attr_accessor :args, :start_day
+  extend self
 
-    def initialize(*args)
-      @args      = args.dup
-      @start_day = day_back 7
-      run(args)
+  def history
+    @history ||= 6
+  end
+
+  def tomorrow
+    @tomorrow ||= Date.today + 1
+  end
+
+  def run(*args)
+    @history = (args.shift.to_i - 1) if args.any?
+    execute
+  end
+
+  def execute
+    tomorrow.step(tomorrow - history, -1) do |day|
+      log = log_for day
+      puts header_for(day - 1)
+      puts log.empty? ? '*no commits' : log
+      puts "\n"
     end
+    exit($?.exitstatus)
+  end
 
-    def self.execute(*args)
-      new(*args).execute
-    end
+  private
 
-    def execute
-      days.each_with_index do |day, i|
-        return if i == days.size - 1
-        puts day_header_for day
-        authors_for(day).each do |author|
-          puts author
-          print commits_for_author_on(author, day)
-        end
-        puts "\n"
-      end
-      exit($?.exitstatus || 0)
-    end
+  def header_for(day)
+    "#{day} (#{day.strftime('%A')})"
+  end
 
-    def run(args)
-      @start_day = day_back args.shift.to_i if args.any?
-    end
-
-    private
-
-    def authors_for(day)
-      %x[ git log --no-merges --format=\"%an\" --since=#{day_before(day)} --until=#{day}
-      ].split("\n").uniq
-    end
-
-    def commits_for_author_on(author, day)
-      %x[ git log --no-merges --author=\"#{author}\" --format=\" - %s\" --since=#{day_before(day)} --until=#{day} ]
-    end
-
-    def days
-      %x[ git log --no-merges --format="%cd" --date=short --since=#{start_day} | sort -u -r
-      ].split("\n").push(start_day)
-    end
-
-    def day_back(count)
-      (Date.today - count.to_i).strftime('%Y-%m-%d')
-    end
-
-    def day_before(day)
-      (Date.parse(day) - 1).strftime('%Y-%m-%d')
-    end
-
-    def day_header_for(day)
-      "#{day} (#{day_word_for(day)})"
-    end
-
-    def day_word_for(day)
-      Date.parse(day).strftime('%A')
-    end
+  def log_for(day)
+    %x[ git shortlog --since=\"#{(day - 1).to_time}\" --until=\"#{day}\" --no-merges --format=\"- %s\" -w\"80,0\" ]
+    .split("\n").reject{|s| s.empty?}.join("\n")
   end
 end
